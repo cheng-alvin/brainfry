c705b1859cd3aea4dff7c6377e3c99fb3c8a4693
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2024-11-08 22:53:03 +1100

    Added basic code for a exit only

--- brainfry.c
+++ brainfry.c
@@ -1,7 +1,146 @@
 #include <jas.h>
+#include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
-  printf("Hello\n");
+char *file;
+
+void error_callback(const char *msg) {
+  fprintf(stderr, "%s: error: %s\n", file, msg);
+}
+
+void *append(void *arr, size_t type_size, size_t *len, void *data,
+             size_t data_size) {
+  size_t new_len = *len + data_size / type_size;
+  arr = realloc(arr, new_len * type_size);
+  memcpy(arr + *len * type_size, data, data_size);
+  *len = new_len;
+  return arr;
+}
+
+int main(int argc, char **argv) {
+
+  if (argc != 2) {
+    fprintf(stderr, "%s: usage: %s <file>\n", argv[0], argv[0]);
+    return 1;
+  }
+
+  file = argv[1];
+
+  FILE *f = fopen(file, "r");
+  if (!f) {
+    fprintf(stderr, "%s: error: could not open file %s\n", argv[0], file);
+    return 1;
+  }
+
+  char ch;
+  size_t len = 1;
+
+  instruction_t *instr = malloc(sizeof(instruction_t));
+  instruction_t start[] = (instruction_t[]){
+      // global _start:
+      (instruction_t){
+          .instr = INSTR_DIR_GLOBAL_LABEL,
+          .operands =
+              (operand_t[]){
+                  (operand_t){.type = OP_MISC, .data = "_start"},
+                  OP_NONE,
+                  OP_NONE,
+                  OP_NONE,
+              },
+      },
+  };
+
+  instruction_t exit[] = (instruction_t[]){
+      // mov rax, 60
+      (instruction_t){
+          .instr = INSTR_MOV,
+          .operands =
+              (operand_t[]){
+                  (operand_t){
+                      .type = OP_R64,
+                      .data = &(enum registers){REG_RAX},
+                  },
+                  (operand_t){
+                      .type = OP_IMM64,
+                      .data = &(uint64_t){60},
+                  },
+                  OP_NONE,
+                  OP_NONE,
+              },
+      },
+      // xor rdi, rdi
+      (instruction_t){
+          .instr = INSTR_XOR,
+          .operands =
+              (operand_t[]){
+                  (operand_t){
+                      .type = OP_R64,
+                      .data = &(enum registers){REG_RDI},
+                  },
+                  (operand_t){
+                      .type = OP_R64,
+                      .data = &(enum registers){REG_RDI},
+                  },
+                  OP_NONE,
+                  OP_NONE,
+              },
+      },
+      // syscall
+      (instruction_t){
+          .instr = INSTR_SYSCALL,
+          .operands =
+              (operand_t[]){
+                  OP_NONE,
+                  OP_NONE,
+                  OP_NONE,
+                  OP_NONE,
+              },
+      },
+  };
+
+  do {
+    ch = fgetc(f);
+    switch (ch) {
+    case '>':
+
+      break;
+
+    case '<':
+      break;
+
+    case '+':
+      break;
+
+    case '-':
+      break;
+
+    case '.':
+      break;
+
+    case ',':
+      break;
+
+    default:
+      break;
+    }
+
+  } while (ch != EOF);
+
+  instr = append(instr, sizeof(instruction_t), &len, start, sizeof(start));
+  instr = append(instr, sizeof(instruction_t), &len, exit, sizeof(exit));
+  buffer_t buf =
+      codegen(MODE_LONG, instr, len * sizeof(instruction_t), CODEGEN_ELF);
+
+  FILE *file_out = fopen("main.o", "wb");
+  size_t written = fwrite(buf.data, sizeof(uint8_t), buf.len, file_out);
+
+  fclose(file_out);
+  fclose(f);
+
+  free(buf.data);
+  free(instr);
+
   return 0;
 }
